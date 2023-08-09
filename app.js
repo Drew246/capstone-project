858729b4afe2da5e154c2e5d008240afbd9f240b
Drew246 <andrew.k.mccollin@gmail.com>
2023-08-08 20:56:01 -0400

    update index and css, add about page file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-(function () {
-    var burger = document.querySelector('.navbar-burger');
-    var menu = document.querySelector('#' + burger.dataset.target);
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
+// function () {
+//   const burger = document.querySelector('.navbar-burger');
+//   const menu = document.querySelector(`#${burger.dataset.target}`);
+//   burger.addEventListener('click', () => {
+//     burger.classList.toggle('is-active');
+//     menu.classList.toggle('is-active');
+//   });
+// }();
