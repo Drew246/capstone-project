96f21f41d0461c2129624078e16b6320da5a36bb
Drew246 <andrew.k.mccollin@gmail.com>
2023-08-10 06:57:27 -0400

    fix js lint ewrrors

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 const speakers = [
-    {
-        name: "John Smith",
-        image: "images/speaker1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis."
-    },
-    {
-        name: "Jane Doe",
-        image: "images/speaker1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis."
-    },
-    {
-        name: "John Smith",
-        image: "images/speaker1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis."
-    },
-    {
-        name: "Jane Doe",
-        image: "images/speaker1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis."
-    },
-    {
-        name: "John Smith",
-        image: "images/speaker1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis."
-    },
-    {
-        name: "Jane Doe",
-        image: "images/speaker1.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis."
-    },
+  {
+    name: 'John Smith',
+    image: 'images/speaker1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis.',
+  },
+  {
+    name: 'Jane Doe',
+    image: 'images/speaker1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis.',
+  },
+  {
+    name: 'John Smith',
+    image: 'images/speaker1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis.',
+  },
+  {
+    name: 'Jane Doe',
+    image: 'images/speaker1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis.',
+  },
+  {
+    name: 'John Smith',
+    image: 'images/speaker1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis.',
+  },
+  {
+    name: 'Jane Doe',
+    image: 'images/speaker1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, quis feugiat facilisis.',
+  },
 ];
-
 
 const speakersSection = document.getElementById('speakers');
 
 function generateSpeakerHTML(speaker) {
-    return `
+  return `
         <div class="column is-4">
             <article class="media">
                 <figure class="media-left pb-5">
@@ -56,51 +55,39 @@ function generateSpeakerHTML(speaker) {
 }
 
 function addSpeakersToSection() {
-    let speakersHTML = '';
+  let speakersHTML = '';
 
-    for(let i = 0; i < speakers.length; i+=2) {
-        let rowHTML = '<div class="columns is-centered">';
+  for (let i = 0; i < speakers.length; i += 2) {
+    let rowHTML = '<div class="columns is-centered">';
 
-        
-        rowHTML += generateSpeakerHTML(speakers[i]);
+    rowHTML += generateSpeakerHTML(speakers[i]);
 
-        
-        if(speakers[i + 1]) {
-            rowHTML += generateSpeakerHTML(speakers[i + 1]);
-        }
-
-        rowHTML += '</div>'; 
-
-        speakersHTML += rowHTML;
+    if (speakers[i + 1]) {
+      rowHTML += generateSpeakerHTML(speakers[i + 1]);
     }
 
-    speakersSection.innerHTML += speakersHTML;
-}
+    rowHTML += '</div>';
 
+    speakersHTML += rowHTML;
+  }
+
+  speakersSection.innerHTML += speakersHTML;
+}
 
 addSpeakersToSection();
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-   
-    const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    
-    if (navbarBurgers.length > 0) {
-  
-    
-      navbarBurgers.forEach(function (el) {
-        el.addEventListener('click', function () {
-  
-          
-          const target = el.dataset.target;
-          const targetElement = document.getElementById(target);
-  
-          
-          el.classList.toggle('is-active');
-          targetElement.classList.toggle('is-active');
-        });
+  if (navbarBurgers.length > 0) {
+    navbarBurgers.forEach((el) => {
+      el.addEventListener('click', () => {
+        const { target } = el.dataset;
+        const targetElement = document.getElementById(target);
+
+        el.classList.toggle('is-active');
+        targetElement.classList.toggle('is-active');
       });
-    }
-  });
-  
+    });
+  }
+});
